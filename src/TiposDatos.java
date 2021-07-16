8f06ef8f906176cc4fc8790573e861beafcdc40d
Braulio <braulio.garrido2209@gmail.com>
2021-07-15 21:38:13 -0500

    Primer commit

---
+++ src/TiposDatos.java
@@ -0,0 +1,23 @@
+package src;
+
+public class TiposDatos {
+	
+	
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			System.out.println("Hola Mundo!");
+			
+			int numero1 = 30;
+			int numero2 = 6;
+			boolean mayor18 = true;
+			String nombre = "Hola";
+			
+			System.out.println(nombre);
+			
+			
+		}
+
+	}
